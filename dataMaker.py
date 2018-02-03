b78c31637565a04f874c8521a01e2bba60e36194
amansawhney <amansawhney@sawhneygames.com>
2018-02-03 12:36:34 -0500

    pushed data to csv

--- dataMaker.py
+++ dataMaker.py
@@ -1,3 +1,4 @@
+
 fileNames = ['english.txt', 'espanol.txt', 'norsk.txt']
 keys = []
 values = []
@@ -7,7 +8,18 @@ for fileName in fileNames:
         keys.append(line.replace("\n", ""))
         values.append(fileName.replace(".txt", ""))
 output_dict = dict(zip(keys, values))
-print output_dict
 
+download_dir = "lang_data.csv" #where you want the file to be downloaded to
 
+csv = open(download_dir, "w")
+#"w" indicates that you're writing strings to the file
+
+columnTitleRow = "lang, word\n"
+csv.write(columnTitleRow)
+
+for key in output_dict.keys():
+	lang = key
+	word = output_dict[key]
+	row = lang + "," + word + "\n"
+	csv.write(row)
 
