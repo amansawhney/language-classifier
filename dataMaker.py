5d3cbcfe58e95e0ed4b83153fe8454087b17847e
amansawhney <amansawhney@sawhneygames.com>
2018-02-03 12:27:36 -0500

    stored lanuages to dic

--- dataMaker.py
+++ dataMaker.py
@@ -1,4 +1,13 @@
-i=0
-f=open('english.txt','r')
-for line in f:
-    print line
+fileNames = ['english.txt', 'espanol.txt', 'norsk.txt']
+keys = []
+values = []
+for fileName in fileNames:
+    f = open(fileName, 'r')
+    for line in f:
+        keys.append(line.replace("\n", ""))
+        values.append(fileName.replace(".txt", ""))
+output_dict = dict(zip(keys, values))
+print output_dict
+
+
+
