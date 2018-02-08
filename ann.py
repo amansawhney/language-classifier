b11a94fb67ad6da08aa00d661fd7de7a9dab5ccb
amansawhney <amansawhney@sawhneygames.com>
2018-02-08 00:49:55 -0500

    Did preprossesing on data

---
+++ ann.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  7 23:42:49 2018
+
+@author: amansawhney
+"""
+
+# Classification template
+
+# Importing the libraries
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+# Importing the dataset
+dataset = pd.read_csv('/home/amansawhney/Development/language-classifier/lang_data.csv')
+x = dataset.iloc[:, 0].values
+y = dataset.iloc[:, 1].values
+print(y[52])
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(y)
+
+X = []
+j = 0
+for i in x:
+    if not (isinstance(i, str)):
+        print(i)
+        print(j)
+        x = np.delete(x, j)
+        y = np.delete(y, j)
+    else:
+        j += 1
+        X.append(list(i))
+
+        
+
+    
+
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.40, random_state = 0)
